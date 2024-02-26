28bbd0c3cdf2c3ef7b701e085c3946ed9636ad21
Tederick Budiono <100257124+erickbdn@users.noreply.github.com>
2024-02-26 14:41:34 +0700

    added optimizeDeps exclude to vite.config.js to fix the internal error 500 problem previously, but then commented it and reload the page and somehow it still works fine? anyways now in dev can open '/' route without internal error 500 and can access sanity studio

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,97 +1,10 @@
 <script>
-    
+    /** @type {import('./$types').PageData} */
     export let data; // Assuming `pets` is the data fetched from Sanity
 
  console.log('Fetched pets:', data); // Log the fetched data to the console
 
 </script>
-<style>
- /* Hero Section Styling */
- .hero-wrapper {
-        display: grid;
-        grid-template-columns: repeat(10, 1fr);
-        grid-template-rows: repeat(10, 1fr);
-        grid-column-gap: 10px;
-        grid-row-gap: 10px;
-        background-color: aqua;
-    }
-
-    .hero-title {
-        grid-area: 3 / 1 / 9 / 11;
-        text-align: center;
-    }
-
-    .image-wrapper-1 { 
-        grid-area: 1 / 7 / 4 / 9; 
-        background-color: blue;
-    }
-
-    .image-wrapper-2 { 
-        grid-area: 5 / 8 / 7 / 11;
-        background-color: blue;
-    }
-
-    .image-wrapper-3 { 
-        grid-area: 2 / 2 / 5 / 4;
-        background-color: blue;
-    }
-
-    .image-wrapper-4 { 
-        grid-area: 7 / 5 / 11 / 7; 
-        background-color: blue;
-    }
-
-    .image-wrapper-5 { 
-        grid-area: 6 / 1 / 9 / 4; 
-        background-color: blue;
-    }
-
-    /* About Section Styling */
-    .about-wrapper {
-        display: grid;
-        grid-template-columns: repeat(10, 1fr);
-        grid-template-rows: repeat(10, 1fr);
-        grid-column-gap: 10px;
-        grid-row-gap: 10px;
-    }
-
-    .about-title {
-        grid-area: 1 / 3 / 4 / 10;
-    }
-
-    .image-wrapper {
-        grid-area: 2 / 1 / 11 / 6;
-    }
-
-    .about-me-name {
-        grid-area: 6 / 6 / 9 / 11;
-    }
-
-    .about-me-description {
-        grid-area: 6 / 6 / 11 / 11;
-    }
-
-    .icons-wrapper {
-        grid-area: 10 / 6 / 11 / 11;
-    }
-
-    /* Nav Section Styling */
-    .nav-section {
-        display: grid;
-        grid-template-columns: repeat(4, 1fr);
-        grid-template-rows: repeat(10, 1fr);
-        grid-column-gap: 10px;
-        grid-row-gap: 10px;
-    }
-
-    .nav-links-container { 
-        grid-area: 2 / 1 / 5 / 5; 
-    }
-
-    .nav-image-container { 
-        grid-area: 1 / 2 / 8 / 4; 
-    }
-</style>
 
     <!-- This is Hero Section -->
     <section class="hero-wrapper">
@@ -139,5 +52,93 @@
             </ul>
         </nav>
     </section>
+
+    <style>
+        /* Hero Section Styling */
+        .hero-wrapper {
+               display: grid;
+               grid-template-columns: repeat(10, 1fr);
+               grid-template-rows: repeat(10, 1fr);
+               grid-column-gap: 10px;
+               grid-row-gap: 10px;
+               background-color: aqua;
+           }
+       
+           .hero-title {
+               grid-area: 3 / 1 / 9 / 11;
+               text-align: center;
+           }
+       
+           .image-wrapper-1 { 
+               grid-area: 1 / 7 / 4 / 9; 
+               background-color: blue;
+           }
+       
+           .image-wrapper-2 { 
+               grid-area: 5 / 8 / 7 / 11;
+               background-color: blue;
+           }
+       
+           .image-wrapper-3 { 
+               grid-area: 2 / 2 / 5 / 4;
+               background-color: blue;
+           }
+       
+           .image-wrapper-4 { 
+               grid-area: 7 / 5 / 11 / 7; 
+               background-color: blue;
+           }
+       
+           .image-wrapper-5 { 
+               grid-area: 6 / 1 / 9 / 4; 
+               background-color: blue;
+           }
+       
+           /* About Section Styling */
+           .about-wrapper {
+               display: grid;
+               grid-template-columns: repeat(10, 1fr);
+               grid-template-rows: repeat(10, 1fr);
+               grid-column-gap: 10px;
+               grid-row-gap: 10px;
+           }
+       
+           .about-title {
+               grid-area: 1 / 3 / 4 / 10;
+           }
+       
+           .image-wrapper {
+               grid-area: 2 / 1 / 11 / 6;
+           }
+       
+           .about-me-name {
+               grid-area: 6 / 6 / 9 / 11;
+           }
+       
+           .about-me-description {
+               grid-area: 6 / 6 / 11 / 11;
+           }
+       
+           .icons-wrapper {
+               grid-area: 10 / 6 / 11 / 11;
+           }
+       
+           /* Nav Section Styling */
+           .nav-section {
+               display: grid;
+               grid-template-columns: repeat(4, 1fr);
+               grid-template-rows: repeat(10, 1fr);
+               grid-column-gap: 10px;
+               grid-row-gap: 10px;
+           }
+       
+           .nav-links-container { 
+               grid-area: 2 / 1 / 5 / 5; 
+           }
+       
+           .nav-image-container { 
+               grid-area: 1 / 2 / 8 / 4; 
+           }
+       </style>
 
 
--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-    ssr: {
-        noExternal: ['sanity', '@sanity/client']
-    }
+    // optimizeDeps: {
+    //     exclude: ['sanity'] // do not pre-bundle some-library
+    //   }
 });
     
